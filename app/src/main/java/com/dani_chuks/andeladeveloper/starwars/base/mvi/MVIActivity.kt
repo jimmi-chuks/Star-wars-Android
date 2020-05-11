5b423bd52f32401b354133211b8f48a4185c36fa
jimmi-chuks <dani.okocha@gmail.com>
2020-05-11 12:07:25 +0100

    Refactor Base MVI fragment and viewmodel

--- app/src/main/java/com/dani_chuks/andeladeveloper/starwars/base/mvi/MVIActivity.kt
+++ app/src/main/java/com/dani_chuks/andeladeveloper/starwars/base/mvi/MVIActivity.kt
@@ -1,14 +1,12 @@
 package com.dani_chuks.andeladeveloper.starwars.base.mvi
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.*
 
 
 @FlowPreview
@@ -19,37 +17,23 @@ abstract class MVIActivity<S, E, A, V : MVIViewmodel<S, E, A>> : AppCompatActivi
 
     abstract fun viewEvents(): Flow<E>
 
-    abstract fun render(state: S)
-
     abstract fun handleAction(action: A)
 
     abstract fun initViewModel()
 
-    private fun subscribeToState() {
-        viewModel.viewState.observe(this, Observer {
-            render(it)
-        })
-    }
-
     private fun subscribeToViewEvents() {
-        lifecycleScope.launch {
-            viewEvents().collect { viewModel.onEvent(it) }
-        }
+        viewEvents().onEach { viewModel.onEvent(it) }
+                .launchIn(lifecycleScope)
     }
 
     @FlowPreview
     private fun subscribeToActions() {
-        lifecycleScope.launch {
-            viewModel.actionChannel
-                    .asFlow()
-                    .collect { handleAction(it) }
-        }
+        viewModel.actionChannel.asFlow()
+                .onEach { handleAction(it) }
+                .launchIn(lifecycleScope)
     }
 
-    fun subscribeToStateEventAndAction() {
-        initViewModel()
-        viewModel.initState()
-        subscribeToState()
+    fun subscribe() {
         subscribeToViewEvents()
         subscribeToActions()
     }
