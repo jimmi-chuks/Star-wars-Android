20e9127e9dee90c0af6db86973e44d4cd686dd01
jimmi-chuks <dani.okocha@gmail.com>
2020-06-08 14:11:13 +0100

    Add helper functions for updating ModelStore intents and actions

--- app/src/main/java/com/dani_chuks/andeladeveloper/starwars/base/mvi/MVIModelStore.kt
+++ app/src/main/java/com/dani_chuks/andeladeveloper/starwars/base/mvi/MVIModelStore.kt
@@ -43,4 +43,43 @@ abstract class ModelStore<S, A>() {
                 store.action?.let { dispatchAction(it) }
                 store.intent.reduce(oldstate)
             })
+
+    fun actionStoreResult(block: () -> A) = storeAction<S,A>(block)
+
+    fun intentStoreResult(block: S.() -> S) = storeIntent<S,A> {  block() }
+
+    fun intentActionStoreUpdate(intentBlock: S.() -> S, actionBlock: (() -> A)?) =
+            StoreResult<S, A>(intent<S> { intentBlock() }, actionBlock?.invoke())
+
+    fun actionFromStateStoreResult(intentBlock: S.() -> S, actionBlock: (S) -> A) =
+            storeActionFromState<S,A>(intentBlock, actionBlock)
+
+
+    /**
+     * Creates a Store Result having an intent where the reduce function is passed as lambda
+     * and dispatches no action
+     */
+    private fun <S, A> storeIntent(block: S.() -> S): StoreResult<S, A> =
+            StoreResult(intent { block() })
+
+    /**
+     * Creates a Store Result having an intent where the reduce function makes no change in state
+     * and dispatches an action obtained by applying the block function on the state to produce an action
+     */
+    private fun <S, A> storeActionFromState(intentBlock: S.() -> S, actionBlock: (S) -> A): StoreResult<S, A> {
+        var state: S? = null
+        val inn = intent<S> {
+            state = this
+            intentBlock(this)
+        }
+        return StoreResult(inn, state?.let{ actionBlock(it) })
+    }
+
+    /**
+     * Creates a Store Result having an intent where the reduce function makes no change in state
+     * and dispatches an action obtained by applying the block function passed as a parameter
+     */
+    private fun <S, A> storeAction(block: () -> A): StoreResult<S, A> {
+        return StoreResult(noChange(), block())
+    }
 }
